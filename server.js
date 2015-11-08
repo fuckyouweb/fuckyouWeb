a093eee5bde36a0fdddd24fc022f575421e653e3
NingYanGuo <gny315@163.com>
2015-11-08 10:44:01 +0800

    20151107

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var COMMENTS_FILE = path.join(__dirname, 'login.json');
+var LOGIN_FILE = path.join(__dirname, 'login.json');
+var COMEON_FILE = path.join(__dirname, 'author/test.jpg');
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -13,17 +14,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/login', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
+  fs.readFile(LOGIN_FILE, function(err, data) {
     res.setHeader('Cache-Control', 'no-cache');
     res.json(JSON.parse(data));
   });
 });
 
 app.post('/api/login', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
+  fs.readFile(LOGIN_FILE, function(err, data) {
     var login = JSON.parse(data);
     login.push(req.body);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(login, null, 4), function(err) {
+    fs.writeFile(LOGIN_FILE, JSON.stringify(login, null, 4), function(err) {
       res.setHeader('Cache-Control', 'no-cache');
       res.json(login);
     });
@@ -32,7 +33,7 @@ app.post('/api/login', function(req, res) {
 
 app.post('/api/comeon',function(req,res){
   alert(3);
-  fs.readFile('author/test.jpg','binary',function(error,file){
+  fs.readFile(COMEON_FILE,'binary',function(error,file){
   if(error){
     response.writeHead(500,{'Content-Type':'text/plain'});
     response.write(error+'\n');
@@ -48,7 +49,7 @@ app.post('/api/comeon',function(req,res){
 
 app.get('/api/comeon', function(req, res) {
   alert(2);
-  fs.readFile(COMMENTS_FILE, function(err, data) {
+  fs.readFile(COMEON_FILE, function(err, data) {
     res.setHeader('Cache-Control', 'no-cache');
     res.json(JSON.parse(data));
   });
